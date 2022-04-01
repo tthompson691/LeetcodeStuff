efb9eb44afca09c609554c488e3e6c84e8e053f3
tthompson2 <tthompson2@eprod.com>
2022-04-01 09:52:39 -0500

    4_1_22 problem update

--- src/reverse_string_easy.py
+++ src/reverse_string_easy.py
@@ -1,3 +1,4 @@
+from wrappers import time_it
 """
 Write a function that reverses a string. The input string is given as an array of characters s.
 You must do this by modifying the input array in-place with O(1) extra memory.
@@ -6,9 +7,11 @@ You must do this by modifying the input array in-place with O(1) extra memory.
 s = ["h","e","l","l","o"]
 cheat = False
 
+@time_it
 def reverse_string(s, cheat):
     if cheat:
         s.reverse()
+        return
 
     l = 0
     r = len(s) - 1
